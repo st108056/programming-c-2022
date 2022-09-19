4844f42b8468638d853ff81fd4cffe9c35cfd205
st108056 <st108056@student.spbu.ru>
2022-09-19 19:43:22 +0300

    Task 10

---
+++ 2022.09.12-Homework-1/Task10/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	cin >> a >> b;
+
+	c = a * pow(b, -1);
+
+	cout << c << endl;
+
+	return EXIT_SUCCESS;
+}
